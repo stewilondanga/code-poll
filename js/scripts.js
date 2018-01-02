f7d584b8c3f41b1b1830601b5c81ad19899b8686
Stewart ilondanga <stilondanga@gmail.com>
2018-01-02 15:23:24 +0300

    add index.html

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,19 @@
 $(document).ready(function() {
-    $("#container div a").click(function() {
-        $(this).parent().animate({
-           width: '+=100px'
-        }, 500);
+  $("#container div a").click(function() {
+    $(this).parent().animate({
+      width: '+=100px'
+    }, 500);
 
-				$(this).prev().html(parseInt($(this).prev().html()) + 1);
-				        return false;
-				    });
-				});
+    $(this).prev().html(parseInt($(this).prev().html()) + 1);
+    return false;
+  });
+});
 
 var navigate = (function() {
-	$('.dd').toggle();
-	$('.dd_btn').click(function() {
-		var dataName = $(this).attr('data-name');
-		$('.dd').hide();
-		$('.' + dataName).toggle();
-	});
+  $('.dd').toggle();
+  $('.dd_btn').click(function() {
+    var dataName = $(this).attr('data-name');
+    $('.dd').hide();
+    $('.' + dataName).toggle();
+  });
 })();
