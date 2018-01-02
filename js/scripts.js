5e7da49d56baeafcfd082bbab2d11975c29320b5
Stewart ilondanga <stilondanga@gmail.com>
2018-01-02 15:23:07 +0300

    add index.html

--- js/scripts.js
+++ js/scripts.js
@@ -4,7 +4,10 @@ $(document).ready(function() {
            width: '+=100px'
         }, 500);
 
-
+				$(this).prev().html(parseInt($(this).prev().html()) + 1);
+				        return false;
+				    });
+				});
 
 var navigate = (function() {
 	$('.dd').toggle();
