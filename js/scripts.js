bfbd7162ce33f5eabc31507177864b4203f499c1
Stewart ilondanga <stilondanga@gmail.com>
2018-01-02 15:22:37 +0300

    add index.html

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,11 @@
+$(document).ready(function() {
+    $("#container div a").click(function() {
+        $(this).parent().animate({
+           width: '+=100px'
+        }, 500);
+
+
+
 var navigate = (function() {
 	$('.dd').toggle();
 	$('.dd_btn').click(function() {
